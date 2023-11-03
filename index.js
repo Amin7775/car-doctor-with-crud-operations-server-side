570d209c2b985ea74bf619112afb51fef8cec04e
Amin7775 <amin.dpi24@gmail.com>
2023-11-04 00:13:18 +0600

    JWT token secret and set token to http only cookie

--- index.js
+++ index.js
@@ -41,10 +41,16 @@ async function run() {
 
     //Auth related api
     app.post('/jwt', async(req,res)=>{
-      const user = req.body;
+      const user=req.body;
       console.log(user)
-      const token = jwt.sign(user, 'secret', { expiresIn: '1h' })
-      res.send(token) 
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure:false,
+        sameSite:'none'
+      })
+      .send({success: true})
     })
 
     // app.get('jwt', async(req,res)=>{
