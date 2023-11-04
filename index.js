abbd9a92797779f491aaa21362cfdd3eb270c24a
Amin7775 <amin.dpi24@gmail.com>
2023-11-04 15:01:07 +0600

    Send token server to client and client to the server side

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json())
+app.use(cookieParser())
 
 
 app.get('/', (req,res)=>{
@@ -47,8 +52,8 @@ async function run() {
       res
       .cookie('token',token,{
         httpOnly:true,
-        secure:false,
-        sameSite:'none'
+        secure:false
+        // SameSite:'none'
       })
       .send({success: true})
     })
@@ -90,6 +95,7 @@ async function run() {
         query = {email: req.query.email}
       }
       console.log(query);
+      console.log("cookies", req.cookies.token)
       const cursor = bookingCollection.find(query)
       const result = await cursor.toArray()
       res.send(result)
